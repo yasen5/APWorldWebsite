463f7238e3e2f3365f33028e946d7a0327270aa2
yasen5 <167659798+yasen5@users.noreply.github.com>
2025-11-26 14:25:41 -1000

    Cleanup timeoutref

--- src/page-transition-provider.tsx
+++ src/page-transition-provider.tsx
@@ -1,54 +1,62 @@
-import { createContext, useContext, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 import { AppPage } from "./AppPageLabels";
 
 interface PageTransitionProps {
-    currentPage: AppPage;
-    setCurrentPage: React.Dispatch<React.SetStateAction<AppPage>>;
-    nextPage: AppPage;
-    setNextPage: React.Dispatch<React.SetStateAction<AppPage>>;
-    transitioning: boolean;
-    setTransitioning: React.Dispatch<React.SetStateAction<boolean>>;
-    goToPage: (page: AppPage) => void;
-  }
-  
+  currentPage: AppPage;
+  setCurrentPage: React.Dispatch<React.SetStateAction<AppPage>>;
+  nextPage: AppPage;
+  setNextPage: React.Dispatch<React.SetStateAction<AppPage>>;
+  transitioning: boolean;
+  setTransitioning: React.Dispatch<React.SetStateAction<boolean>>;
+  goToPage: (page: AppPage) => void;
+}
+
 const PageTransitionContext = createContext<PageTransitionProps | undefined>(
-    undefined
+  undefined
 );
-  
+
 export const usePageTransitionContext = () => {
-    const context: PageTransitionProps | undefined = useContext(
+  const context: PageTransitionProps | undefined = useContext(
     PageTransitionContext
-    );
-    if (!context) {
+  );
+  if (!context) {
     throw new Error(
-        "usePageTransitionContext must be used within a PageTransitionProvider"
+      "usePageTransitionContext must be used within a PageTransitionProvider"
     );
-    }
-    return context;
+  }
+  return context;
 };
 
-export const PageTransitionProvider: React.FC<{ children: React.ReactNode }> = ({
-    children
-}) => {
-    const [currentPage, setCurrentPage] = useState<AppPage>(AppPage.START_SCREEN);
-    const [nextPage, setNextPage] = useState<AppPage>(AppPage.EXPLANATION);
-    const [transitioning, setTransitioning] = useState<boolean>(false);
-    const timeoutRef = useRef<number | null>(null);
+export const PageTransitionProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [currentPage, setCurrentPage] = useState<AppPage>(AppPage.START_SCREEN);
+  const [nextPage, setNextPage] = useState<AppPage>(AppPage.EXPLANATION);
+  const [transitioning, setTransitioning] = useState<boolean>(false);
+  const timeoutRef = useRef<number | null>(null);
 
-    const goToPage = (page: AppPage) => {
+  const goToPage = (page: AppPage) => {
     if (transitioning) return;
     setTransitioning(true);
     setNextPage(page);
     if (timeoutRef.current) clearTimeout(timeoutRef.current);
     timeoutRef.current = setTimeout(() => {
-        setCurrentPage(page);
-        setTransitioning(false);
+      setCurrentPage(page);
+      setTransitioning(false);
     }, 1000);
-    };
+  };
 
-    return (
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  return (
     <PageTransitionContext.Provider
-        value={{
+      value={{
         currentPage,
         setCurrentPage,
         nextPage,
@@ -56,9 +64,9 @@ export const PageTransitionProvider: React.FC<{ children: React.ReactNode }> = (
         transitioning,
         setTransitioning,
         goToPage,
-        }}
+      }}
     >
-        {children}
+      {children}
     </PageTransitionContext.Provider>
-    );
+  );
 };
